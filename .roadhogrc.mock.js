f8f9eafd85add6d57067328e9537673ac0fafd78
luocong2016 <695393722@qq.com>
2017-09-19 14:06:52 +0800

    init

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,8 @@
 
-export default {
-};
+const mock = {}
+
+require('fs').readdirSync(require('path').join(__dirname + '/mock')).forEach(function (file) {
+  Object.assign(mock,require('./mock/' + file))
+})
+
+module.exports = mock
